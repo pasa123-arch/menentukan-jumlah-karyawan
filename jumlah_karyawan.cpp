38aba82cc9ceaf91b8609c38b82cb0e37876e39d
pasa123-arch <24051214141@mhs.unesa.ac.id>
2024-10-07 12:02:09 +0700

    Add files via upload

---
+++ jumlah_karyawan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int jumlahKaryawan;
+
+    cout << "Masukan Jumlah karyawan: "; 
+    cin >> jumlahKaryawan;
+
+    cout << "Jumlah karyawan yang dimasukan adalah: " << jumlahKaryawan << endl; 
+
+    return 0;
+}
